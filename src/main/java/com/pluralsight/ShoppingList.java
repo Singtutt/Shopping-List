2b2fe75143f36de0850f41f235eec98091badc18
Sing Tuttanon <singtuttanon@gmail.com>
2023-10-06 15:08:28 -0400

    Completed ShoppingList App

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+//  Display of 10 items from Grocery list
+        System.out.println("Grocery Shopping List:");
+        System.out.println("1. Apples");
+        System.out.println("2. Bananas");
+        System.out.println("3. A2 Milk");
+        System.out.println("4. Kings Hawaiian Bread");
+        System.out.println("5. Eggs");
+        System.out.println("6. Pasta Noodles");
+        System.out.println("7. Rice");
+        System.out.println("8. Whole Chicken");
+        System.out.println("9. Spinach");
+        System.out.println("10. Tomatoes");
+//  System.out.println("~messages~") = To display a given message that is prompted
+    }
+}
